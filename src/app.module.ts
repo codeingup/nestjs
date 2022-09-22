3e05b1c4b83b8070108a3e3a169e33d979a3f4a4
codeingup <l17876189271@gmail.com>
2022-09-22 18:13:05 +0800

    'update'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest_database',
+      synchronize: true, //不应在生产环境中使用
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
