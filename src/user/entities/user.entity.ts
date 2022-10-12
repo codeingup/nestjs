f14da76fe77872c83d82bfab603ecee344e09c9c
codeingup <l17876189271@gmail.com>
2022-10-12 18:27:05 +0800

    backup

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,20 +1,22 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User{
-    @PrimaryGeneratedColumn('uuid')
-    id:string
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({comment:"昵称"})
-    nickName:string
+  @Column({ comment: '昵称' })
+  nickName: string;
 
-    @Column({comment:"性别 0:女  1:男 2:未知"})
-    sex:number
+  @Column({ comment: '性别 0:女  1:男 2:未知' })
+  sex: number;
 
-    @Column({comment:'手机号码'})
-    phone:number
+  @Column({ comment: '手机号码' })
+  phone: number;
 
-    @Column({comment:"密码"})
-    password:string
+  @Column({ comment: '密码' })
+  password: string;
+
+  @Column({ comment: '邮箱' })
+  email: string;
 }
