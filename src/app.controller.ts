ac1e94c24033132048f3a01658c68b951d1dd5dd
codeingup <l17876189271@gmail.com>
2023-02-07 11:45:41 +0800

    删除不必要的参数

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Param() params): string {
+  getHello(): string {
     return this.appService.getHello();
   }
   @Post()
